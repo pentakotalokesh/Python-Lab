11269aae9d1e4301bf8326d6ca48db48deed078a
pentakota lokesh <pentakotalokesh67@gmail.com>
2021-09-27 15:14:11 +0530

    3:13

--- 9.py
+++ 9.py
@@ -4,3 +4,11 @@ program should then check to see if the strings are of the same length. If they 
 the program should print an appropriate message and exit. If they are of the same length, the program should alternate the characters of the two strings. For example, if
 the user enters abcde and ABCDE the program should print out AaBbCcDdEe
 """
+print("Both Strings Could be Equal")
+str1=input("Enter String 1:")
+str2=input("Enter String 2:")
+if len(str1)!=len(str2):
+    print("Two Strings Not Equal")
+else:
+    for i in range(len(str1)):
+        print(str1[i],str2[i],end=" ")
