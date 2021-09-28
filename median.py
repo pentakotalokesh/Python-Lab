cd7de74c2f5cfbc9823fa973d41c3adbc810315b
pentakota lokesh <pentakotalokesh67@gmail.com>
2021-09-28 12:32:01 +0530

    12:31

---
+++ median.py
@@ -0,0 +1,9 @@
+list=[55,23,45,12,3,5,7,20]
+list.sort()
+length
+if list%2==0:
+    index=list//2
+    print("Median is ",list[index])
+else:
+    index=list//2
+    print("Median is ",list[index])
